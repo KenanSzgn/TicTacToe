70a8977cdd48c302875b3677d1c1599d12b52ee7
KenanSzgn <s0559666@htw-berlin.de>
2019-05-31 01:53:28 +0200

    I need to look in line 21 in the Main Class and find a solution

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -13,17 +13,24 @@ import java.util.Scanner;
 public class TicTacToe {
     Scanner in = new Scanner(System.in);
     Random randomNumber = new Random();
-    // Array fÃƒÂ¼r das Spielbrett
+    // Array for the gameboard
     char[] board = new char[9];
 
+    // This Method prints the gameboard
+    // German:
     // Diese Methode gibt ihr das übergebene Spielbrett aus
     // board ist ein Array aus 10 Strings (Index 0 wird ignoriert)
-    public void printBoard(String[] board) {
+    public void printBoard(char[] board) {
         System.out.println(board[7] + " | " + board[8] + " | " + board[9] + " | ");
         System.out.println(board[4] + " | " + board[5] + " | " + board[6] + " | ");
         System.out.println(board[1] + " | " + board[2] + " | " + board[3] + " | ");
     }
 
+
+    // Lets the player select the desired letter.
+    // Returns a list with the player letter first and
+    // the computer letter as the second element
+    //German:
     // Lässt den Spieler den gewünschten Buchstaben wählen.
     // Gibt eine Liste mit dem Spielerbuchstaben als erstem und
     // dem Computerbuchstaben als zweitem Element zurück
@@ -32,8 +39,11 @@ public class TicTacToe {
         char[] arr;
         while (letter != 'x' || letter != 'o') {
             System.out.println("Do you want to be X or O?");
+            letter = in.next().toUpperCase().charAt(0);
         }
-        letter = in.next().toUpperCase().charAt(0);
+        // The first element of the list is the letter of the gamer, the
+        // second element is the letter of the computers.
+        // German:
         // Das erste Element der Liste ist der Buchstabe des Spielers, das
         // zweite der Buchstabede des Computers.
         if (letter == 'X') {
@@ -43,6 +53,8 @@ public class TicTacToe {
         }
     }
 
+    // Randomly choose who has the first move
+    // German:
     // Wählt zufällig aus wer den ersten Zug hat
     public String whoGoesFirst() {
         int r = randomNumber.nextInt(2);
@@ -54,11 +66,16 @@ public class TicTacToe {
         }
     }
 
+    // Set your figure
+    // German:
     // Setze deine Figur
     public void makeMove(char[] board, char letter, int move) {
         board[move] = letter;
     }
 
+    // The method accepts a game board and the player letter and
+    // returns true if the player has won.
+    // German:
     // Die Methode nimmt ein Spielbrett und den Spielerbuchstaben entgegen und
     // gibt true zurück, wenn der Spieler gewonnen hat.
     public boolean isWinner(char[] board, char letter) {
@@ -84,6 +101,9 @@ public class TicTacToe {
 
     }
 
+    // For the AI to make changes to a temporary copy of the board to plan a move.
+    // Copy the game board and return the copy
+    // German:
     // Für die KI, damit sie Änderungen an einer temporären Kopie des Spielbretts
     // machen kann, um somit einen Zug zu planen
     // Kopiert die Spielbrettliste und gibt die Kopie zurück
@@ -95,6 +115,8 @@ public class TicTacToe {
         return boardCopy;
     }
 
+    // Returns true if the given move is possible on the passed board
+    // German:
     // Gibt true zurück, wenn der übergebene Zug auf dem übergebenen
     // Spielbrett möglich ist
     public boolean isSpaceFree(char[] board, int move) {
@@ -105,9 +127,14 @@ public class TicTacToe {
         }
     }
 
+    // Lets the player enter a move
+    // German:
     // Lässt den Spieler einen Zug eingeben
     public int getPlayerMove(char[] board) {
         int move = 0;
+        // As long as the user does not make a correct entry between 1 and 9 and
+        // as long as the corresponding field is not free, ask where the user wants to put the figure
+        // German:
         // Solange der User keine Korrekte Eingabe zwischen 1 und 9 macht und solange das entsprechende
         // Feld nicht frei ist, frage wo der User setzen will.
         while (move < 1 && move > 9 || isSpaceFree(board, move) != true) {
@@ -117,6 +144,9 @@ public class TicTacToe {
         return move;
     }
 
+    // Returns a valid move from the submitted list on the passed board.
+    // Returns null if there is no valid move
+    // German:
     // Gibt einen gültigen Zug von der übergebenen Liste
     // auf dem übergebenen Spielbrett zurück
     // Gibt null zurück, wenn es keinen gültigen Zug gibt
@@ -129,6 +159,8 @@ public class TicTacToe {
                 possibleMoves.add(movesList[i]);
             }
         }
+        // If the list is empty, issue a random index
+        // German:
         // Falls die Liste leer ist, gebe einen zufälligen index heraus
         if (possibleMoves.size() != 0) {
             return possibleMoves.get(rand.nextInt(possibleMoves.size()));
@@ -143,51 +175,63 @@ public class TicTacToe {
         } else {
             playerLetter = 'X';
         }
+        // Algorithm for the Tic Tac Toe AI. Check first
+        // if a win with the next move is possible
+        // German:
         // Algorithmus für die Tic Tac Toe KI
         // Prüft als erstes ob ein Sieg mit dem nächsten Zug möglich ist
         char[] boardCopy = new char[10];
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             boardCopy = getBoardCopy(board);
-            if (isSpaceFree(boardCopy, i) == true){
+            if (isSpaceFree(boardCopy, i) == true) {
                 makeMove(boardCopy, computerLetter, i);
-                if (isWinner(boardCopy, computerLetter) == true){
+                if (isWinner(boardCopy, computerLetter) == true) {
                     return i;
                 }
             }
         }
 
+        // Check if the player could win with his next turn and block this move
+        //German:
         // Prüft, ob der Spiler mit seinem nächsten Zug gewinnen könnte
         // und blockiert diesen Zug
-
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             boardCopy = getBoardCopy(board);
-            if (isSpaceFree(boardCopy, i) == true){
+            if (isSpaceFree(boardCopy, i) == true) {
                 makeMove(boardCopy, playerLetter, i);
-                if (isWinner(boardCopy, playerLetter) == true){
+                if (isWinner(boardCopy, playerLetter) == true) {
                     return i;
                 }
             }
         }
 
+        // Try to occupy one of the corners if one is free
+        // German:
         // Versucht eine der Ecken zu belegen wenn eine frei ist
         Integer move = chooseRandomMoveFromList(board, new int[]{1, 3, 7, 9});
-        if (move != null){
+        if (move != null) {
             return move;
         }
 
+        // Try to occupy the center when it is free
+        // German:
         // Versucht die Mitte zu belegen, wenn sie frei ist
-        if (isSpaceFree(board, 5) == true){
+        if (isSpaceFree(board, 5) == true) {
             return 5;
         }
 
+        // Make a move on one of the sides
+        // German:
         // Macht einen Zug auf einer der Seiten
         return chooseRandomMoveFromList(board, new int[]{2, 4, 6, 8});
     }
 
+    // Returns true if all game fields are occupied, otherwise false.
+    // German:
     // Gibt true zurück, wenn alle Spielfelder belegt sind, andernfalls false.
-    public boolean isBoardFull(char[] board){
-        for (int i = 0; i < 10; i++){
-            if (isSpaceFree(board, i) == true){
+    public boolean isBoardFull(char[] board) {
+        for (int i = 0; i < 10; i++) {
+            if (isSpaceFree(board, i) == true) {
                 return false;
             }
         }
--- src/TicTacToeTest.java
+++ src/TicTacToeTest.java
@@ -8,33 +8,67 @@
 public class TicTacToeTest {
     public static void main(String[] args) {
         TicTacToe ob = new TicTacToe();
-        String[] arr = new String[10];
-        ob.printBoard(arr);
-    // start ab hier
-
         char[] playerLetter;
         char[] computerLetter;
+        char playerLetterChar = ' ';
+        char computerLetterChar = ' ';
         System.out.println("Welcome to Tic-Tac-Toe!");
 
         boolean hilfe = true;
         while (hilfe) {
             // setze das Spielbrett zurück
             char[] theBoard = new char[]{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+            // QUESTION:
+            // in Python you can use multiple assigments and
+            // it looks like this: playerLetter, computerLetter = inputPlayerLetter()
+            // i need a alternative solution for java
             playerLetter = ob.inputPlayerLetter();
             computerLetter = ob.inputPlayerLetter();
+
             String turn = ob.whoGoesFirst();
-            System.out.println("The "+turn+" will go first.");
+            System.out.println("The " + turn + " will go first.");
 
+            boolean gameIsPlaying = true;
+            while (gameIsPlaying) {
+                // The turn of the Player
+                if (turn == "Player") {
+                    ob.printBoard(theBoard);
+                    int move = ob.getPlayerMove(theBoard);
+                    ob.makeMove(theBoard, playerLetterChar, move);
 
-           boolean gameIsPlaying = true;
-           while (gameIsPlaying){
+                    if (ob.isWinner(theBoard, playerLetterChar) == true) {
+                        ob.printBoard(theBoard);
+                        System.out.println("Hooray! You have won the game!");
+                        gameIsPlaying = false;
+                    } else {
 
-           }
+                        if (ob.isBoardFull(theBoard) == true) {
+                            System.out.println("The game is a tiel!");
+                            break;
+                        } else {
+                            turn = "Computer";
+                        }
+                    }
+                } else {
+                    // The turn of the Computers
+                    int move = ob.getComputerMove(theBoard, computerLetterChar);
+                    ob.makeMove(theBoard, computerLetterChar, move);
+
+                    if (ob.isWinner(theBoard, computerLetterChar) == true) {
+                        ob.printBoard(theBoard);
+                        System.out.println("The Computer has beaten you! You lose.");
+                        gameIsPlaying = false;
+                    } else {
+
+                        if (ob.isBoardFull(theBoard) == true) {
+                            System.out.println("The game is a tiel!");
+                            break;
+                        } else {
+                            turn = "Player";
+                        }
+                    }
+                }
+            }
         }
-
-
-
-
     }
-
 }
