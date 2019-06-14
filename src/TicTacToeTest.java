b19c4814c4a2fde8d8ccf560e3c05ce5cb31c2f7
KenanSzgn <s0559666@htw-berlin.de>
2019-06-14 17:16:51 +0200

    // ich benötige doch die boardCopy variante         // 1. kann ich als computer mit dem nächsten Zug gewinnnen         // 2. // Prüft, ob der Spiler mit seinem nächsten Zug gewinnen könnte         //// und blockiert diesen Zug
            // 3. Versucht eine der Ecken zu belegen wenn eine frei ist
            // 4. Versucht die Mitte zu belegen, wenn sie frei

--- src/TicTacToeTest.java
+++ src/TicTacToeTest.java
@@ -8,15 +8,20 @@
 public class TicTacToeTest {
 
     public static void main(String[] args) {
-        char[] arr = new char[10];
-
         TicTacToe ob = new TicTacToe();
-        System.out.println(ob.whoGoesFirst());
+        char[] board = new char[10];
 
-        ob.printBoard(arr);
-        ob.inputPlayerLetter();
+        ob.printBoard(board);
+        char playerLetter = ob.inputPlayerLetter();
+        String first = ob.whoGoesFirst();
+        // Nur zur Probe
+        System.out.println(first);
 
-
+        if(first == "Player"){
+            ob.getPlayerMove(board);
+        }else{
+            ob.getComputerMove(board,playerLetter);
+        }
 
     }
 }
